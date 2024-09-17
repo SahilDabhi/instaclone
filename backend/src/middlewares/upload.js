9d556c349137fede88bcb424ddabbb9aa8f95ed6
Sahil Dabhi <sahilbdabhi@gmail.com>
2024-09-17 23:01:42 +0530

    added file specification

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -11,6 +12,23 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
+  fileFilter: (req, file, cb) => {
+    const allowedImageTypes = /jpeg|jpg|png|gif/;
+    const allowedVideoTypes = /mp4|mov|avi|mkv/;
+
+    const extname =
+      allowedImageTypes.test(path.extname(file.originalname).toLowerCase()) ||
+      allowedVideoTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype =
+      allowedImageTypes.test(file.mimetype) ||
+      allowedVideoTypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb(new Error("Can upload photo or video only"));
+  },
 });
 
 export default upload;
